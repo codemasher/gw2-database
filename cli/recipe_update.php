94e680e80de560cd108a3c0eeaf1276c3bdaea32
smiley <smiley@chillerlan.net>
2014-03-03 18:45:49 +0100

    small changes reflecting the changes in mysqli.inc

--- cli/recipe_update.php
+++ cli/recipe_update.php
@@ -21,22 +21,22 @@ $n = "\n";
 // first of all refresh the recipe IDs
 $data = gw2_api_request('recipes.json');
 if(is_array($data) && isset($data['recipes'])){
-	echo count($data['recipes']).' items in recipes.json.'.$n;
-	if($stmt = mysqli_prepare($db, 'INSERT IGNORE INTO `gw2_recipes` (`recipe_id`) VALUES (?)')){
-		mysqli_stmt_bind_param($stmt, "i", $id);
-		foreach($data['recipes'] as $recipe){
-			$id = $recipe;
-			mysqli_stmt_execute($stmt);
-		}
-		mysqli_stmt_close($stmt);
-		echo 'refresh done.'.$n;
+	$values = array();
+	$time = time();
+	foreach($data['recipes'] as $id){
+		$values[] = array($id, $time);
 	}
+	sql_multi_row_insert('INSERT IGNORE INTO `gw2_recipes` (`recipe_id`, `date_added`) VALUES (?, ?)', $values, 'ii');
+	echo 'refresh done.'.$n.count($data['recipes']).' items in recipes.json.'.$n;
 }
 
 $q = sql_query('SELECT `recipe_id` FROM `gw2_recipes` WHERE `updated` = 0 ORDER BY `gw2_recipes`.`recipe_id`');
 if(is_array($q) && count($q) > 0){
 	$err = array();
 	$sql = 'UPDATE `gw2_recipes` SET `output_id` = ?, `output_count` = ?, `disciplines`= ?, `rating` = ?, `type` = ?, `from_item` = ?, `ing_id_1` = ?, `ing_count_1` = ?, `ing_id_2` = ?, `ing_count_2` = ?, `ing_id_3` = ?, `ing_count_3` = ?, `ing_id_4` = ?, `ing_count_4` = ?, `data` = ?, `updated` = ?, `update_time` = ? WHERE `recipe_id` = ?';
+
+	// i don't use the multiline insert over here as it would blow up the system memory
+	// also this way we insert each line as soon as we get the data from the API
 	if($stmt = mysqli_prepare($db, $sql)){
 		/** @noinspection PhpUndefinedVariableInspection */
 		mysqli_stmt_bind_param($stmt, 'iiiisiiiiiiiiisiii', $output_id, $output_count, $disciplines, $rating, $type, $from_item, $ing_id_1, $ing_count_1, $ing_id_2, $ing_count_2, $ing_id_3, $ing_count_3, $ing_id_4, $ing_count_4, $data, $updated, $update_time, $recipe_id);
