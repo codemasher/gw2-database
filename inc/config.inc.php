8f68011457ff75b9821ec892c0a4ec4248eed119
smiley <smiley@chillerlan.net>
2014-03-04 16:05:16 +0100

    added config file

---
+++ inc/config.inc.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * config.inc.php
+ * created: 04.03.14
+ */
+
+
+// mysql connection data
+$mysql = array(
+	'server'   => 'localhost',
+	'user'     => 'user',
+	'password' => 'password',
+	'dbname'   => 'dbname'
+);
+
+define('TABLE_ITEMS', 'gw2_items');
+define('TABLE_COLORS', 'gw2_colors');
+define('TABLE_EVENTS', 'gw2_events');
+define('TABLE_MAPS', 'gw2_maps');
+define('TABLE_RECIPES', 'gw2_recipes');
+
+?>
