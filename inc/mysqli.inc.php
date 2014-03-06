9ecf84f42e89b641e3c80b1530d140346c96619a
smiley <smiley@chillerlan.net>
2014-03-06 12:18:44 +0100

    moved connection data to config.inc.php

--- inc/mysqli.inc.php
+++ inc/mysqli.inc.php
@@ -4,25 +4,17 @@
  * created: 07.07.13
  */
 
-// mysql connection data
-$mysql = array(
-	'server'   => 'localhost',
-	'user'     => 'user',
-	'password' => 'password',
-	'dbname'   => 'dbname'
-);
-
 $db = mysqli_init();
 
 // connect to the db
 if(!mysqli_real_connect($db, $mysql['server'], $mysql['user'], $mysql['password'], $mysql['dbname'])){
 	// note: you sould not expose sql errors to the public on a production system. never. ever.
-	exit('Could not connect to the database: '.mysqli_connect_errno().' - '.mysqli_connect_error());
+	exit('Could not connect to the database. ');// .mysqli_connect_errno().' - '.mysqli_connect_error()
 }
 
 // set the connection dataset
 if(!mysqli_set_charset($db, 'utf8')){
-	exit('Error loading character set utf8: '.mysqli_error($db));
+	exit('Error loading character set utf8. ');// .mysqli_error($db)
 }
 
 /**
