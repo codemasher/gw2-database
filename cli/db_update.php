9077905c1435a345b13427631a033d757661f7da
smiley <smiley@chillerlan.net>
2014-03-06 14:10:10 +0100

    attribute combination names

--- cli/db_update.php
+++ cli/db_update.php
@@ -12,8 +12,11 @@ date_default_timezone_set('Europe/Berlin');
 set_time_limit(0);
 mb_internal_encoding('UTF-8');
 
+require_once '../inc/config.inc.php';
 require_once '../inc/mysqli.inc.php';
 require_once '../inc/request.inc.php';
+require_once '../inc/variables.inc.php';
+require_once '../inc/attributes.inc.php';
 
 $n = "\n";
 
@@ -25,23 +28,23 @@ if(is_array($data) && isset($data['items'])){
 	foreach($data['items'] as $item){
 		$values[] = array($item, $time);
 	}
-	sql_multi_row_insert('INSERT IGNORE INTO `gw2_items` (`id`, `date_added`) VALUES (?, ?)', $values, 'ii');
+	sql_multi_row_insert('INSERT IGNORE INTO '.TABLE_ITEMS.' (`id`, `date_added`) VALUES (?, ?)', $values, 'ii');
 	echo 'refresh done.'.$n.count($data['items']).' items in items.json.'.$n;
 }
 
 // ok, now the nasty part - you may want to set `updated` to 0 before a full update
-#sql_query('UPDATE `gw2_items` SET `updated` = 0');
+#sql_query('UPDATE '.TABLE_ITEMS.' SET `updated` = 0');
 
-$q = sql_query('SELECT `id` FROM `gw2_items` WHERE `updated` = 0 ORDER BY `gw2_items`.`id`');
+$q = sql_query('SELECT `id` FROM '.TABLE_ITEMS.' WHERE `updated` = 0 ORDER BY '.TABLE_ITEMS.'.`id`');
 if(is_array($q) && count($q) > 0){
 	$err = array();
-	$sql = 'UPDATE `gw2_items` SET `signature` = ?, `file_id` = ?, `rarity` = ?, `weight` = ?, `type` = ?, `subtype` = ?, `unlock_type` = ?, `level` = ?, `value` = ?, `pvp` = ?, `attr1` = ?, `attr2` = ?, `attr3` = ?, `unlock_id` = ?, `name_de` = ?, `name_en` = ?, `name_es` = ?, `name_fr` = ?, `desc_de` = ?, `desc_en` = ?, `desc_es` = ?, `desc_fr` = ?, `data_de` = ?, `data_en` = ?, `data_es` = ?, `data_fr` = ?, `updated` = ?, `update_time` = ?  WHERE `id` = ?';
+	$sql = 'UPDATE '.TABLE_ITEMS.' SET `signature` = ?, `file_id` = ?, `rarity` = ?, `weight` = ?, `type` = ?, `subtype` = ?, `unlock_type` = ?, `level` = ?, `value` = ?, `pvp` = ?, `attr1` = ?, `attr2` = ?, `attr3` = ?, `attr_name` = ?, `unlock_id` = ?, `name_de` = ?, `name_en` = ?, `name_es` = ?, `name_fr` = ?, `desc_de` = ?, `desc_en` = ?, `desc_es` = ?, `desc_fr` = ?, `data_de` = ?, `data_en` = ?, `data_es` = ?, `data_fr` = ?, `updated` = ?, `update_time` = ?  WHERE `id` = ?';
 
 	// i don't use the multiline insert over here as it would blow up the system memory
 	// also this way we insert each line as soon as we get the data from the API
 	if($stmt = mysqli_prepare($db, $sql)){
 		/** @noinspection PhpUndefinedVariableInspection */
-		mysqli_stmt_bind_param($stmt, 'sisssssiiisssissssssssssssiii', $signature, $file_id, $rarity, $weight, $type, $subtype, $unlock_type, $level, $value, $pvp, $attr1, $attr2, $attr3, $unlock, $name_de, $name_en, $name_es, $name_fr, $desc_de, $desc_en, $desc_es, $desc_fr, $json_de, $json_en, $json_es, $json_fr, $updated, $update_time, $item_id);
+		mysqli_stmt_bind_param($stmt, 'sisssssiiissssissssssssssssiii', $signature, $file_id, $rarity, $weight, $type, $subtype, $unlock_type, $level, $value, $pvp, $attr1, $attr2, $attr3, $attr_name, $unlock, $name_de, $name_en, $name_es, $name_fr, $desc_de, $desc_en, $desc_es, $desc_fr, $json_de, $json_en, $json_es, $json_fr, $updated, $update_time, $item_id);
 		foreach($q as $i){
 			$starttime = microtime(true);
 			$data_de = gw2_api_request('item_details.json?item_id='.$i['id'].'&lang=de');
@@ -49,20 +52,25 @@ if(is_array($q) && count($q) > 0){
 			$data_es = gw2_api_request('item_details.json?item_id='.$i['id'].'&lang=es');
 			$data_fr = gw2_api_request('item_details.json?item_id='.$i['id'].'&lang=fr');
 
-			switch($data_en['type']){
-				case 'CraftingMaterial' : $t = 'crafting_material'; break;
-				case 'MiniPet' 			: $t = 'mini_pet'; break;
-				case 'UpgradeComponent' : $t = 'upgrade_component'; break;
-				default					: $t = strtolower($data_en['type']); break;
-			}
-
-			switch(true){
-				case isset($data_en[$t]['recipe_id'])	: $unlock_id = $data_en[$t]['recipe_id']; break;
-				case isset($data_en[$t]['color_id'])	: $unlock_id = $data_en[$t]['color_id']; break;
-				default: $unlock_id = 0; break;
-			}
-
 			if(isset($data_de['item_id']) && isset($data_en['item_id']) && isset($data_es['item_id']) && isset($data_fr['item_id'])){
+				switch($data_en['type']){
+					case 'CraftingMaterial' : $t = 'crafting_material'; break;
+					case 'MiniPet'          : $t = 'mini_pet'; break;
+					case 'UpgradeComponent' : $t = 'upgrade_component'; break;
+					default                 : $t = strtolower($data_en['type']); break;
+				}
+
+				switch(true){
+					case isset($data_en[$t]['recipe_id']) : $unlock_id = $data_en[$t]['recipe_id']; break;
+					case isset($data_en[$t]['color_id'])  : $unlock_id = $data_en[$t]['color_id']; break;
+					default                               : $unlock_id = 0; break;
+				}
+
+				$com = array('', array());
+				if(isset($data_en[$t]['infix_upgrade']['attributes'])){
+					$com = attribute_combination($data_en[$t]['infix_upgrade']);
+				}
+
 				$signature = $data_en['icon_file_signature'];
 				$file_id = $data_en['icon_file_id'];
 				$rarity = $data_en['rarity'];
@@ -73,9 +81,10 @@ if(is_array($q) && count($q) > 0){
 				$level = intval($data_en['level']);
 				$value = intval($data_en['vendor_value']);
 				$pvp = in_array('Pvp',$data_en['game_types']) && in_array('PvpLobby',$data_en['game_types']) ? 1 : 0;
-				$attr1 = isset($data_en[$t]['infix_upgrade']['attributes'][0]['attribute']) ? ($data_en[$t]['infix_upgrade']['attributes'][0]['attribute']) : '';
-				$attr2 = isset($data_en[$t]['infix_upgrade']['attributes'][1]['attribute']) ? ($data_en[$t]['infix_upgrade']['attributes'][1]['attribute']) : '';
-				$attr3 = isset($data_en[$t]['infix_upgrade']['attributes'][2]['attribute']) ? ($data_en[$t]['infix_upgrade']['attributes'][2]['attribute']) : '';
+				$attr1 = isset($com[1][0]) ? $com[1][0] : '';
+				$attr2 = isset($com[1][1]) ? $com[1][1] : '';
+				$attr3 = isset($com[1][2]) ? $com[1][2] : '';
+				$attr_name = $com[0];
 				$unlock = $unlock_id;
 				$name_de = str_replace(array(chr(194).chr(160), '  '), ' ', $data_de['name']);
 				$name_en = str_replace(array(chr(194).chr(160), '  '), ' ', $data_en['name']);
