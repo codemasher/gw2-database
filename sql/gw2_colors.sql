1df0e45285277706f9ac92d394a610e101bd6e3c
smiley <smiley@chillerlan.net>
2014-08-03 00:23:15 +0200

    removed unnessecary field `id`

--- sql/gw2_colors.sql
+++ sql/gw2_colors.sql
@@ -1,7 +1,6 @@
 CREATE TABLE IF NOT EXISTS `gw2_colors` (
-  `id` SMALLINT(4) unsigned NOT NULL AUTO_INCREMENT,
-  `color_id` SMALLINT(4) unsigned NOT NULL,
-  `item_id` MEDIUMINT(6) unsigned NOT NULL,
+  `color_id` smallint(5) unsigned NOT NULL,
+  `item_id` mediumint(6) unsigned NOT NULL,
   `tone` enum('red','orange','yellow','green','blue','purple','brown','gray','none') COLLATE utf8_bin NOT NULL DEFAULT 'none',
   `set` enum('starter','common','uncommon','rare','special','none') COLLATE utf8_bin NOT NULL DEFAULT 'none',
   `material` tinytext COLLATE utf8_bin NOT NULL, -- will be changed to enum once we get this via the API
@@ -13,5 +12,5 @@ CREATE TABLE IF NOT EXISTS `gw2_colors` (
   `cloth` tinytext COLLATE utf8_bin NOT NULL,
   `leather` tinytext COLLATE utf8_bin NOT NULL,
   `metal` tinytext COLLATE utf8_bin NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+  PRIMARY KEY (`color_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
