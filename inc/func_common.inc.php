75ee5bf88abb03357135c17de5a45f9df8ae7483
smiley <smiley@chillerlan.net>
2015-08-20 15:13:09 +0200

    added array_diff_assoc_recursive() and array_sort_recursive()

--- inc/func_common.inc.php
+++ inc/func_common.inc.php
@@ -163,12 +163,49 @@ function write_log($line, $logfile, $dateformat = '[Y-m-d H:i:s]', $echo = false
 }
 
 /**
- * @param string $old
- * @param string $new
+ * @param array $arr1
+ * @param array $arr2
+ * @param bool  $identical
  *
- * @return bool
+ * @return array
+ * @link http://php.net/manual/function.array-diff-assoc.php#111675
  */
-function diff($old, $new){
-	return sha1($old) !== sha1($new);
+function array_diff_assoc_recursive(array $arr1, array $arr2, $identical = false){
+	$diff = $identical ? array_diff_key($arr2, $arr1) : [];
+	foreach($arr1 as $key => $value){
+		if(is_array($value)){
+			if(!isset($arr2[$key]) || !is_array($arr2[$key])){
+				$diff[$key] = $value;
+			}
+			else{
+				$new_diff = array_diff_assoc_recursive($value, $arr2[$key], $identical);
+				if(!empty($new_diff)){
+					$diff[$key] = $new_diff;
+				}
+			}
+		}
+		else if(!array_key_exists($key, $arr2) || $arr2[$key] !== $value){
+			$diff[$key] = $value;
+		}
+	}
+
+	return $diff;
+}
+
+/**
+ * @param array $array
+ *
+ * @return array
+ */
+function array_sort_recursive(array $array){
+	array_multisort($array);
+	ksort($array);
+	foreach($array as $key => $value){
+		if(is_array($value)){
+			$array[$key] = array_sort_recursive($value);
+		}
+	}
+
+	return $array;
 }
 
