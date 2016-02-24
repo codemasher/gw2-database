7ce0d1e82c67fb693f4382880a55d99c7a4adcd4
smiley <smiley@chillerlan.net>
2016-02-24 23:54:29 +0100

    added name fields

--- sql/gw2_items_temp.sql
+++ sql/gw2_items_temp.sql
@@ -1,13 +1,19 @@
 CREATE TABLE IF NOT EXISTS `gw2_items_temp` (
   `id`            INT(10) UNSIGNED    NOT NULL,
-  `de`            TEXT                NOT NULL,
-  `en`            TEXT                NOT NULL,
-  `es`            TEXT                NOT NULL,
-  `fr`            TEXT                NOT NULL,
-  `zh`            TEXT                NOT NULL,
+  `blacklist`     TINYINT(1)          NOT NULL DEFAULT '0',
+  `name_de`       VARCHAR(255)        NOT NULL,
+  `name_en`       VARCHAR(255)        NOT NULL,
+  `name_es`       VARCHAR(255)        NOT NULL,
+  `name_fr`       VARCHAR(255)        NOT NULL,
+  `name_zh`       VARCHAR(255)        NOT NULL,
+  `data_de`       TEXT                NOT NULL,
+  `data_en`       TEXT                NOT NULL,
+  `data_es`       TEXT                NOT NULL,
+  `data_fr`       TEXT                NOT NULL,
+  `data_zh`       TEXT                NOT NULL,
   `updated`       TINYINT(1) UNSIGNED NOT NULL DEFAULT '0',
-  `response_time` TIMESTAMP           NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
+  `create_time`   TIMESTAMP           NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `response_time` TIMESTAMP           NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 )
   ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4
